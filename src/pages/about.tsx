9362d07d6de514f29308f343c788645521ee8478
thdy <thdy00@gmail.com>
2020-12-19 02:17:49 +0900

    Aboutページの修正

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,22 +9,23 @@ const Page: NextPage = () => {
       <PageSEO title="About" path="/about" />
       <ContentWrapper>
         <section className="about">
-          <h1 className="about__title">About me</h1>
-          <img src="thdy.jpg">
+          <h1 className="about__title">About</h1>
+          <img src="http://thdy.jp/avatars/thdy.jpg" width="300"></img>
           <div className="about__body">
-            <p>
-              ・thdy (@thdy5, <a href="https://twitter.com/thdy5">Twitter</a>)
-              ・加来 慎太郎 (Shintaro Kaku, <a href="https://www.linkedin.com/in/shintaro-kaku-975b59a8/">LinkedIn</a>)
-              ・広島在住のリモートワーカー
-              ・Japan Okta User Group 運営 (<a href="https://okta.connpass.com/"></a>JOUG)
-              ・業務パッケージの保守開発とサポートエンジニアを経験した後に、国内外にグループ会社を持つホールディングカンパニーの情報システム部にてグループ全体のIT統制や各種ソリューションの導入、運用に従事。2020年11月よりヘイ株式会社、コーポレートエンジニア。
-              ・<a href="https://zenn.dev/thdy/">Zenn.dev</a>
-              ・<a href="https://note.com/thdy/">note</a>
-              ・<a href="https://thdy.hatenablog.jp/">hatena blog</a>
-              ・<a href="https://qiita.com/THDY/">Qiita</a>
-              ・<a href="https://lapras.com/public/MJDDR5B">LAPRAS</a>
-              ・ご意見・ご質問、お仕事のご相談・ご依頼はTwitterの <a href="https://twitter.com/thdy5">@thdy5</a> 宛にDMをお送りください。
-            </p>
+            <ul>
+            <br></br>
+            <li>・thdy (@thdy5, <a href="https://twitter.com/thdy5">Twitter</a>)</li>
+            <li>・加来 慎太郎 (Shintaro Kaku, <a href="https://www.linkedin.com/in/shintaro-kaku-975b59a8/">LinkedIn</a>)</li>
+            <li>・広島在住のリモートワーカー</li>
+            <li>・Japan Okta User Group 運営 (<a href="https://okta.connpass.com/"></a>JOUG)</li>
+            <li>・プロダクト開発、サポートエンジニアを経て情シス/コーポレートエンジニアとして活動中</li>
+            <li>・<a href="https://zenn.dev/thdy/">Zenn.dev</a></li>
+            <li>・<a href="https://note.com/thdy/">note</a></li>
+            <li>・<a href="https://thdy.hatenablog.jp/">hatena blog</a></li>
+            <li>・<a href="https://qiita.com/THDY/">Qiita</a></li>
+            <li>・<a href="https://lapras.com/public/MJDDR5B">LAPRAS</a></li>
+            <li>・ご意見・ご質問、お仕事のご相談・ご依頼はTwitterの <a href="https://twitter.com/thdy5">@thdy5</a> 宛にDMをお送りください。</li>
+            </ul>
           </div>
           <div className="about__actions">
             <LinkBackHome />
